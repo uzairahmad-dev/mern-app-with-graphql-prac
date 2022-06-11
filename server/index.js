6a090b590964d78477db39b0d6633daf9c5947c6
Uzair Ahmad <uzairahmad.dev@gmail.com>
2022-06-11 18:26:56 +0500

    Connect with a mongoDB

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const port = process.env.PORT || 5000;
+
+const app = express();
+connectDB();
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`server running on ${port}`.yellow.underline.bold));
